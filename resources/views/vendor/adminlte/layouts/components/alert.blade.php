1ca8988dcb6d80f4332b4149ceb33a8570bde976
9vimu9 <9wimu9@gmail.com>
2017-11-25 23:16:08 +0530

    test

---
+++ resources/views/vendor/adminlte/layouts/components/alert.blade.php
@@ -0,0 +1,5 @@
+<div class="alert alert-{{$color}} alert-dismissible">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+  <h4><i class="icon fa fa-{{$icon}}"></i> {{$title}}</h4>
+  {{$message}}
+</div>
