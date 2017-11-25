0b1aeed14dd7f809f9ab1bbc282b5553a354707a
9vimu9 <9wimu9@gmail.com>
2017-11-25 23:01:21 +0530

    Added my project

--- resources/views/vendor/adminlte/home.blade.php
+++ resources/views/vendor/adminlte/home.blade.php
@@ -9,7 +9,7 @@
 	<div class="container-fluid spark-screen">
 		<div class="row">
 			<div class="col-md-12">
-
+ff
 				<!-- Default box -->
 				<div class="box">
 					<div class="box-header with-border">
@@ -23,7 +23,82 @@
 						</div>
 					</div>
 					<div class="box-body">
-						{{-- {{ trans('adminlte_lang::message.logged') }}. Start creating your amazing application! --}}
+						<form method="post" class="form-horizontal">
+
+ <div class="form-group">
+ <label class="col-sm-3 control-label">Alphabets</label>
+ <div class="col-sm-6">
+ <input type="text" class="form-control" required data-parsley-pattern="^[a-zA-Z ]+$" placeholder="Type something" />
+ </div>
+ </div>
+
+
+ <div class="form-group">
+ <label class="col-sm-3 control-label">Equal To</label>
+ <div class="col-sm-3">
+ <input type="password" id="pass2" class="form-control" required data-parsley-length="[6, 10]" data-parsley-trigger="keyup" placeholder="Password" />
+        </div>
+ <div class="col-sm-3">
+ <input type="password" class="form-control" required data-parsley-equalto="#pass2" data-parsley-trigger="keyup" placeholder="Re-Type Password" />
+        </div>
+ </div>
+
+ <div class="form-group">
+ <label class="col-sm-3 control-label">E-Mail</label>
+ <div class="col-sm-6">
+ <input type="email" class="form-control" required data-parsley-type="email" data-parsley-trigger="keyup" placeholder="Enter a valid e-mail" />
+        </div>
+ </div>
+
+ <div class="form-group">
+ <label class="col-sm-3 control-label">URL</label>
+ <div class="col-sm-6">
+ <input type="url" class="form-control" required data-parsley-type="url" placeholder="URL" />
+ </div>
+ </div>
+
+ <div class="form-group">
+ <label class="col-sm-3 control-label">Number</label>
+ <div class="col-sm-6">
+ <input type="text" class="form-control" required data-parsley-type="number" placeholder="Enter only numbers" />
+ </div>
+ </div>
+
+ <div class="form-group">
+ <label class="col-sm-3 control-label">Alphanumeric</label>
+ <div class="col-sm-6">
+ <input type="text" class="form-control" required data-parsley-type="alphanum" placeholder="Enter alphanumeric value" />
+ </div>
+ </div>
+
+ <div class="form-group">
+ <label class="col-sm-3 control-label">Select Box</label>
+ <div class="col-sm-6">
+ <select class="form-control" required>
+      <option value="" selected="selected"> - Select - </option>
+      <option>First Value</option>
+      <option>Second Value</option>
+             <option>Third Value</option>
+      <option>Fourth Value</option>
+ </select>
+ </div>
+ </div>
+
+ <div class="form-group">
+ <label class="col-sm-3 control-label">Textarea</label>
+ <div class="col-sm-6">
+ <textarea required class="form-control"></textarea>
+ </div>
+ </div>
+
+ <div class="form-group">
+ <div class="col-sm-offset-3 col-sm-9 m-t-15">
+ <button type="submit" class="btn btn-primary">Submit</button>
+ <button type="reset" class="btn btn-default m-l-5">Cancel</button>
+ </div>
+ </div>
+
+</form
 					</div>
 					<!-- /.box-body -->
 				</div>
---
+++ resources/views/vendor/adminlte/layouts/partials/callouts.blade.php
@@ -0,0 +1,4 @@
+<div class="callout callout-{{$color}}">
+ <h4>{{$title}}</h4>
+ <p>Follow the steps to continue to payment.</p>
+</div>
